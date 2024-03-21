fc0cd7fb59ea3f4f034bb2dcd560320e1933440c
Vicrrs <enricofermi2019@gmail.com>
2024-03-21 03:18:29 -0300

    operações matematicas

---
+++ scripts/operacoes_matematicas.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let soma = 5 + 10;
+    let subtracao = 95.5 - 4.3;
+    let multiplicacao = 4 * 30;
+    let divisao = 56.7 / 32.2;
+    let resto = 43 % 5;
+
+    println!("Soma: {}", soma);
+    println!("Subtração: {}", subtracao);
+    println!("Multiplicação: {}", multiplicacao);
+    println!("Divisão: {}", divisao);
+    println!("Resto: {}", resto);
+}
