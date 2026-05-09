ff9ba47632bd8a392d51618779adf29b4be18729
vicrrs <victorroza22@gmail.com>
2026-05-08 23:32:10 -0300

    exercicie var02

---
+++ exercises/01_variables/variables2.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    // variáveis precisam ser inicializadas antes de serem usadas
+    let x = 10;
+
+    if x==10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
